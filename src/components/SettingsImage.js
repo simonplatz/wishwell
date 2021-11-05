9e1762994a618ab7ce5eb10b7e39164afcaae49a
simonplatz <sibec19@student.sdu.dk>
2021-11-05 12:45:22 +0100

    information on the user in settings
    User information

--- src/components/SettingsImage.js
+++ src/components/SettingsImage.js
@@ -1,32 +1,72 @@
 import React from "react";
-import { View, Image, StyleSheet, Text } from "react-native";
+import { View, Image, StyleSheet, Text, StatusBar, SafeAreaView } from "react-native";
+import {FlatList} from "react-native-gesture-handler";
+
 
 export default SettingsImage => {
+  const infomrationrender = ({item}) => (
+    <Information title = {item.title} />
+  );
   return (
     <View style={style.container}>
-      <Image
-        style={{
-          borderColor: "black",
-          borderWidth: 1,
-          height: 125,
-          width: 125,
-          borderRadius: 125/2
-        }}
-        source={require("../../assets/wishwlll.png")}
-      />
-      <Text>Joakim billede</Text>
-    </View>
+    <Image
+      style={{
+        borderColor: "black",
+        borderWidth: 1,
+        height: 125,
+        width: 125,
+        borderRadius: 125/2,
+      }}
+      source={require("../../assets/wishwlll.png")}
+    />
+      <SafeAreaView style={style.container}>
+        <FlatList 
+        data={DATA} 
+        renderItem={infomrationrender}
+        keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+      </View>
   );
 }
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Joakim'
+  },
+  {
+    id:'3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title:'email@email.com'
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: '17-07-1997'
+  }
+
+]
+
+const Information = ({title}) => (
+  <View style={style.item}>
+    <Text style={style.text}>{title}</Text>
+  </View>
+)
+
 const style = StyleSheet.create({
   container: {
-    display: "flex",
-    position: "absolute",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    top: 0,
-    left: 5,
-    overflow: "hidden"
+    width: 250,
+    flexDirection: "row",
+    paddingLeft: 20,
+    paddingTop: 10,
+  },
+  item:{
+    padding: 3,
+    marginVertical: 2,
+    marginHorizontal: 16,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  text:{
+    fontSize: 20
   }
 });
