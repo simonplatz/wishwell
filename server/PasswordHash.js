42a570f0e6b455e352b1ec44611d1c3d01510e37
LucasAjander <luaja19@student.sdu.dk>
2021-11-16 08:26:59 +0100

    Update PasswordHash.js

--- server/PasswordHash.js
+++ server/PasswordHash.js
@@ -4,12 +4,13 @@ const saltRounds = 10;
 module.exports = {hashPassword, comparePassword}
 
 
-
+//Hashes a given password based on the saltRounds(cost factor) 
 function hashPassword(password){
     const hash = bcrypt.hashSync(password, saltRounds)
     return hash
 }
 
+//Compares a given password, with an encrypted password from the database
 function comparePassword(password, passwordDB){
     const compare = bcrypt.compareSync(password, passwordDB)
     return compare 
